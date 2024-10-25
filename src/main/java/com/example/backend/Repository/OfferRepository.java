7ceb18d28dcf6d7424f7df8a2d19adf39f1ef6c9
GarvGyanani <garvgyanani101@gmail.com>
2024-10-25 10:56:57 +0530

    h

--- src/main/java/com/example/backend/Repository/OfferRepository.java
+++ src/main/java/com/example/backend/Repository/OfferRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface OfferRepository extends MongoRepository<Offer, String> {
-    List<Offer> findByShopId(String shopId); 
+    List<Offer> findByShopId(String shopId); // Find offers by shopId
+}
--- src/main/java/com/example/backend/Repository/PromotionRepository.java
+++ src/main/java/com/example/backend/Repository/PromotionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface PromotionRepository extends MongoRepository<Promotion, String> {
-    List<Promotion> findByShopId(String shopId); 
+    List<Promotion> findByShopId(String shopId); // Find promotions by shop ID
+}
